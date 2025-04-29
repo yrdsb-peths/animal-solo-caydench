a637aeb1cc5ce5c04b4538c129543a93c25bf7f0
caydench <349581918@gapps.yrdsb.ca>
2025-04-29 09:03:15 -0400

    Add Apple

---
+++ Apple.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The apple, food for elephant.
+ * 
+ * @author Chiu 
+ * @version April 2025
+ */
+public class Apple extends Actor
+{
+    public Apple() {
+        setImage("images/apple1.png");
+    }
+    
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
