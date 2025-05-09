077971c595289931d11d68dd7cab970094c280a1
caydench <349581918@gapps.yrdsb.ca>
2025-05-08 21:03:05 -0400

    increase score and speed up game

--- Elephant.java
+++ Elephant.java
@@ -19,9 +19,9 @@ public class Elephant extends Actor
     public void act()
     {
         if(Greenfoot.isKeyDown("left")) {
-            move (-1);
+            move (-3);
         } else if (Greenfoot.isKeyDown("right")) {
-            move(1);
+            move(3);
         }
         
         eat();
@@ -32,6 +32,7 @@ public class Elephant extends Actor
             removeTouching(Apple.class);
             MyWorld world = (MyWorld)getWorld();
             world.createApple();
+            world.increaseScore();
         }
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -1,16 +1,24 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    public int scoreCount = 0;
+    Label score;
+    
     public MyWorld() {
         super(600, 400, 1);
         
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
         
-        Label score = new Label(0,100);
+        score = new Label(0,100);
         addObject(score, 40, 50);
         
         createApple();
+    }
+    
+    public void increaseScore() {
+        scoreCount++;
+        score.setValue(scoreCount);
     }
     
     public void createApple() {
