4b25bea76bb6ec59d7ea96c1f59ae6e15c263651
caydench <349581918@gapps.yrdsb.ca>
2025-05-09 12:32:50 -0400

    Add Instructions

--- TitleScreen.java
+++ TitleScreen.java
@@ -16,14 +16,49 @@ public class TitleScreen extends World
     public TitleScreen() {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
-        addObject(titleLabel, getWidth()/2, getHeight()/2);
+
+        addObject(titleLabel, getWidth()/2, 85);
+        prepare();
     }
-    
+
     public void act() {
         if(Greenfoot.isKeyDown("space")) {
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Elephant elephant = new Elephant();
+        addObject(elephant,549,87);
+        elephant.setLocation(495,86);
+        elephant.setLocation(517,83);
+        Label label = new Label("Use <-- and --> to Move", 40);
+        addObject(label,310,202);
+        label.setLocation(327,202);
+        Label label2 = new Label("Press <space> to Start", 40);
+        addObject(label2,289,287);
+        label2.setLocation(384,287);
+        label2.setLocation(283,284);
+        label.setLocation(305,209);
+        label.setLocation(284,186);
+        label.setLocation(295,189);
+        elephant.setLocation(530,209);
+        label.setLocation(385,194);
+        label2.setLocation(368,273);
+        label.setLocation(249,202);
+        label2.setLocation(203,293);
+        label2.setLocation(318,309);
+        label2.setLocation(284,299);
+        label2.setLocation(252,292);
+        label2.setLocation(248,291);
+        elephant.setLocation(495,253);
+        elephant.setLocation(523,255);
+        elephant.setLocation(519,253);
     }
 }
