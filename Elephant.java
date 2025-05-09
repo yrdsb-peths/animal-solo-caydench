6570269c5783b757261689434ab00187f815d44a
caydench <349581918@gapps.yrdsb.ca>
2025-05-09 09:19:49 -0400

    Speed up elephant animation

--- Elephant.java
+++ Elephant.java
@@ -11,7 +11,9 @@ public class Elephant extends Actor
     GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
     GreenfootImage[] idleRight = new GreenfootImage[8];
     GreenfootImage[] idleLeft = new GreenfootImage[8];
+    
     String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
     
     public Elephant() {
         for(int i = 0; i < idleRight.length; i++) {
@@ -25,11 +27,18 @@ public class Elephant extends Actor
             idleLeft[i].scale(100, 100);
         }
         
+        animationTimer.mark();
+        
         setImage(idleRight[0]);
     }
     
     int imageIndex = 0;
     public void animateElephant() {
+        if(animationTimer.millisElapsed() < 100) {
+            return;
+        }
+        animationTimer.mark();
+        
         if(facing.equals("right")) {
             setImage(idleRight[imageIndex]);
             imageIndex = (imageIndex + 1) % idleRight.length;
